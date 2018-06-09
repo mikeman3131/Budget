1919c297fe94b3e90e633bc58939cfdebb27e116
MSI\J300 <chilinwu@itri.org.tw>
2018-06-09 17:28:28 +0800

    pass test case: same month and different month

---
+++ BudgetCalculator/BugetService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator
+{
+    public class BugetService
+    {
+        private IRespository _respository;
+
+        public BugetService(IRespository respository)
+        {
+            _respository = respository;
+        }
+
+
+        public int GetBuget(DateTime startDateTime, DateTime endDateTime)
+        {
+            var bugets = this._respository.GetBudget();
+            if ((startDateTime.Year == endDateTime.Year) &&
+                (startDateTime.Month == endDateTime.Month))
+            {
+               
+                var buget = bugets.Where(x => x.Month == startDateTime.Month).FirstOrDefault();
+                var diff_days = endDateTime.Subtract(startDateTime).TotalDays+1;
+                
+
+                return (int)(buget.dayOfMonth() * diff_days);
+            }
+
+            if ((startDateTime.Month != endDateTime.Month))
+            {
+                var diff_days = endDateTime.Subtract(startDateTime).TotalDays+1;
+                var enumerable = Enumerable.Range(startDateTime.Month, endDateTime.Month);
+                var budgets= bugets.Where(o => enumerable.Contains(o.Month)).ToList();
+                var sum= budgets.Take(budgets.Count - 1).Sum(o => o.Money);
+
+                var lastMonthDays =
+                endDateTime.Subtract(
+                new DateTime(endDateTime.Year, endDateTime.Month, 1)).Days+1;
+                sum += lastMonthDays * bugets.Last().dayOfMonth();
+                return sum;
+            }
+
+            return 10;
+        }
+    }
+}
--- BudgetCalculator/UnitTest1.cs
+++ BudgetCalculator/UnitTest1.cs
@@ -1,14 +1,98 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
 
 namespace BudgetCalculator
 {
     [TestClass]
-    public class UnitTest1
+    public class BudgetCalculatorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private BugetService _bugetService;
+        private IRespository _respository = Substitute.For<IRespository>();
+
+        private List<Buget> dBugets = new List<Buget>()
         {
+            new Buget
+            {
+                Year = 2018,
+                Month = 06,
+                Money = 300
+            },
+            new Buget
+            {
+                Year = 2018,
+                Month = 08,
+                Money = 310
+            }
+        };
+
+        [TestMethod]
+        public void WholeMonth()
+        {
+            // Arrange
+
+            _respository.GetBudget().Returns(dBugets);
+
+            _bugetService = new BugetService(_respository);
+
+            var expected = 10;
+
+            var startDateTime = new DateTime(2018, 6, 1);
+            var endDateTime = new DateTime(2018, 6, 1);
+
+            // Act
+            var actual = _bugetService.GetBuget(startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void WholeMonth2()
+        {
+            // Arrange
+
+            _respository.GetBudget().Returns(dBugets);
+
+            _bugetService = new BugetService(_respository);
+
+            var expected = 150;
+
+            var startDateTime = new DateTime(2018, 6, 1);
+            var endDateTime = new DateTime(2018, 6, 15);
+
+            // Act
+            var actual = _bugetService.GetBuget(startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void test_20180601_20180715()
+        {
+            // Arrange
+
+            _respository.GetBudget().Returns(dBugets);
+
+            _bugetService = new BugetService(_respository);
+
+            var expected = 450;
+
+            var startDateTime = new DateTime(2018, 6, 1);
+            var endDateTime = new DateTime(2018, 7, 15);
+
+            // Act
+            var actual = _bugetService.GetBuget(startDateTime, endDateTime);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public interface IRespository
+    {
+        List<Buget> GetBudget();
     }
 }
